defad7e37f7d829a7da16d038438c65454de06a2
Sign <ccx01@126.com>
2014-07-21 23:33:34 +0800

    取消单独skill文件
    之前为啥要将skill分离开已经忘掉了，难道是为了在不同的关卡使用不同的技能？
    这样似乎也不错，但暂时还是先合并，否则到时候每个角色都加一套对应的skill也挺麻烦

--- js/characters/ochi.js
+++ js/characters/ochi.js
@@ -32,15 +32,44 @@
 			this.angle = angle;
 		}
 
-		ochi.skill = {}
-
-		module.load("get_skill", [{
-				"name": "skill",
-				"url": "js/effect/skill.js"
-			}], function(mod){
-				ochi.skill.Qkey = mod.skill.Qkey;
-				ochi.skill.Wkey = mod.skill.Wkey;
-			});
+		ochi.skill = (function(){
+			var I = {}
+			var Qkey = mod.model();
+				Qkey.sprite = mod.sprite("ui/mark.png", 0, 0, 22, 20, ready);
+				Qkey.move = function(){
+					if((this.vx > 0 && this.x > this.dx) || (this.vx < 0 && this.x < this.dx) || (this.vy > 0 && this.y > this.dy) || (this.vy < 0 && this.y < this.dy)){
+						this.active = false;
+					} else {
+						this.x += this.vx;
+						this.y += this.vy;
+					}
+				}
+				Qkey.update = function() {
+					this.img.ani([
+						[0,0,22,20],
+						[0,20,22,20]
+					],100);
+					this.move();
+					(game.time - this.time > this.age) && (this.active = false);
+				}
+				Qkey.add = function(age, x, y, dx, dy, speed, time){
+					this.active = true;
+					this.name = "Qkey";
+					this.age = age || 0;
+					this.x = x || 0;
+					this.y = y || 0;
+					this.dx = dx || 0;
+					this.dy = dy || 0;		
+					this.angle = Math.atan2(this.dy - this.y, this.dx - this.x);
+					this.vx = Math.cos(this.angle) * speed || 0;
+					this.vy = Math.sin(this.angle) * speed || 0;
+					this.time = game.time || 0;
+					game.objectPool.push(this);
+				}
+			I.Qkey = Qkey;
+			I.Wkey = Qkey;
+			return I;
+		}());
 
 		ochi.cmd = function(listener) { //cmd drived by the event listener, listener set by chapter
 			switch(listener){
